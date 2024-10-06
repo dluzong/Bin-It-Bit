64428da611e100dd734b4b948039d81deb8b46bb
emiklap <emiklap@gmail.com>
2024-10-06 04:17:57 -0400

    got the cutscene to work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import time
 
 from cutscenes import *
 from button import Button
@@ -14,7 +15,7 @@ class Game:
         pygame.display.set_caption("Bin-It-Bit Game")
         self.clock = pygame.time.Clock()
         
-        self.gameStateManager = GameStateManager('menu')
+        self.gameStateManager = GameStateManager('menu') #the app should start at the menu
         self.menu = Menu(self.screen, self.gameStateManager)
         self.cutscene = Cutscene(self.screen, self.gameStateManager)
         
@@ -35,13 +36,44 @@ class Cutscene:
     def __init__(self, display, gameStateManager):
         self.display = display
         self.gameStateManager = gameStateManager
+
     def run(self):
+        SCREEN = self.display
+        background_3 = pygame.image.load("assets/doomsday-three-bkg.png")
+        background_2 = pygame.image.load("assets/doomsday-two-bkg.png")
+        background_1 = pygame.image.load("assets/doomsday-one-bkg.png")
+        background_0 = pygame.image.load("assets/doomsday-zero-bkg.png")
+
         cutscene_manager = CutsceneManager()
-        cutscene_manager.add_scene(DialogueScene('It was a dark and stormy night...', 5))
-        cutscene_manager.add_scene(DialogueScene('Suddenly, a shot rang out!', 4))
-        cutscene_manager.add_scene(DialogueScene('The maid screamed.', 3))
-        cutscene_manager.add_scene(DialogueScene('And all was silent...', 5))
+        SCREEN.blit(background_3, (0, 0))
+        cutscene_manager.add_scene(DialogueScene('It was an ordinary day in Union Square.\nOur protagonist, Jane Doe, is on a\nstroll when she looks up and notices...', 4))
+        cutscene_manager.add_scene(DialogueScene('The doomsday clock was finally \ncounting down to...', 3))
+        cutscene_manager.add_scene(DialogueScene('...', 2))
+        cutscene_manager.add_scene(DialogueScene('...zero??', 2))
+        cutscene_manager.add_scene(DialogueScene('The climate change apocalypse \nhas finally begun?!', 3))
+        cutscene_manager.add_scene(DialogueScene('As a consequence of not taking \nenough precautions, Jane Doe and \nthe other inhabitants of Earth are \nforced to collect trash unless they \nwant to be buried under it forever!!', 5))
         cutscene_manager.start()
+
+        while self.gameStateManager.get_state() == 'cutscene':
+            if cutscene_manager.return_scene_index() == 1:
+                SCREEN.blit(background_2, (0, 0))
+            elif cutscene_manager.return_scene_index() == 2:
+                SCREEN.blit(background_1, (0, 0))
+            elif cutscene_manager.return_scene_index() == 3:
+                SCREEN.blit(background_0, (0, 0))
+            cutscene_manager.update()  # Continuously update the current cutscene
+            cutscene_manager.draw(self.display)  # Draw the current cutscene on the screen
+            pygame.display.update()  # Refresh display to show updated visuals
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # Check if all scenes are finished and then change state
+            if cutscene_manager.active_scene is None:
+                self.gameStateManager.set_state('menu')  # CHANGE TO GAMEPLAY !!!!!!
+
 
 class Menu:
     def __init__(self, display, gameStateManager):
